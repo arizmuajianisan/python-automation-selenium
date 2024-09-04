9cbd74f32d1023012b1b8099deb31c7f5f028840
arizmuajianisan <arizmuajianisan@gmail.com>
2024-09-04 16:14:39 +0700

    [fix] sequence task, exit process if in current month, process the hyper after all files downloaded

--- main.selenium.py
+++ main.selenium.py
@@ -7,12 +7,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 import time
+import calendar
 import os
 import sys
 from datetime import datetime
 import zipfile
 from merge_csv import merge_csv_files
 from cleaner import clean_directories
+from hyper_exporter import export_to_hyper
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,11 +26,14 @@ username = os.getenv("USER")
 password = os.getenv("PASS")
 project_dir = os.path.dirname(os.path.abspath(__file__))  # Root directory of the code
 download_dir = project_dir  # Set download directory to the code directory
+zip_dir = os.path.join(project_dir, "zip_file")  # Path to the 'zip_file' subdirectory
 
 
 # Initialize browser
 def initialize_browser():
     chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("start-maximized")
     chrome_options.add_argument(
         "--unsafely-treat-insecure-origin-as-secure=http://192.168.147.74/",
     )
@@ -36,7 +41,7 @@ def initialize_browser():
     chrome_options.add_experimental_option(
         "prefs",
         {
-            "download.default_directory": download_dir,  # Set download directory to the code directory
+            "download.default_directory": zip_dir,  # Set download directory to the code directory
             "download.prompt_for_download": False,  # Disable the prompt for download
             "safebrowsing.enabled": False,  # Allow potentially dangerous downloads
             "safebrowsing.disable_download_protection": True,  # Disable download protection
@@ -47,6 +52,7 @@ def initialize_browser():
     )
 
     browser = webdriver.Chrome(options=chrome_options)
+    browser.set_page_load_timeout(3000000)
     browser.get(url)
     return browser
 
@@ -62,7 +68,7 @@ def login(browser):
 
 
 # Navigate and perform tasks
-def navigate_and_perform_tasks(browser):
+def navigate_and_perform_tasks(browser, date_selection_start, date_selection_end=None):
     """
     Step 1: Navigate to the Data Output page and perform tasks.
     """
@@ -143,13 +149,14 @@ def navigate_and_perform_tasks(browser):
         sys.exit()
 
     """
-    Step 7: Select the only 'Completed' form then click NEXT button
+    Step 7: Select the only 'Completed' form
     """
     try:
         select_element = browser.find_element(By.ID, "EditReferStatus")
         # print("Select option: %s\n", select_element.text) # Debug list optons
         select = Select(select_element)
         select.select_by_visible_text("Completed")
+        time.sleep(2)
         print("Step 7: Success to select the status of form to 'Completed'")
     except Exception as e:
         print(
@@ -158,112 +165,137 @@ def navigate_and_perform_tasks(browser):
         sys.exit()
 
     """
-    Step 8a: Select the form with selected time range
+    Step 8: Click the search button to set the date selection
 
     Using date_selection to input the date start
     currently the selected data start from -1 month back
     """
     try:
-        date_selection = "2024/08/28"  # Change this starting from
         WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.CLASS_NAME, "img_search"))
         ).click()
-        print("Step 8a Search button clicked")
-        time.sleep(2)
-
-        WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located(
-                (By.XPATH, "//*[@id='SearchInfo_RegistDateFrom']")
-            )
-        ).send_keys(f"{date_selection}")
-        print("Step 8a Inserted date: %s" % date_selection)
-        time.sleep(2)
-
-        WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located(
-                (By.XPATH, "//*[@id='SearchInfo_RegistDateFrom']")
-            )
-        ).send_keys(Keys.ESCAPE)
-
-        WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.ID, "conmasSearchButton"))
-        ).click()
-        time.sleep(2)
-
-        WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located(
-                (By.XPATH, "//*[@id='SearchDialog']/div/div[1]/span/img")
-            )
-        ).click()
-
-        print("Step 8a Click the search button")
+        print("Step 8 Search button clicked")
     except Exception as e:
-        print(f"Step 8a An error occurred while clicking the search button: {e}")
-        sys.exit()
+        print(f"Step 8 Search button click failed: {e}")
 
     """
-    Step 8b: Click the all checkbox
-    This will drive to select all form, uncomment if this section is used
-    THis 8b only select on the checbox available on this window
-    """
-    # try:
-    #     WebDriverWait(browser, 10).until(
-    #         EC.element_to_be_clickable((By.ID, "HeadCbx"))
-    #     ).click()
-    #     print("Step 8b Success to select the checkbox 'ID'")
-    # except Exception as e:
-    #     print(f"Step 8b An error occurred while selecting the checkbox: {e}")
-    #     sys.exit()
-
-    """
-    Step 8c: Click all the checbox from this output
+    Step 9: Insert date started and date end
     """
     try:
         WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.ID, "IsAllOutput"))
-        ).click()
-        print("Step 8c Success to select the checkbox 'ID'")
+            EC.presence_of_element_located(
+                (By.XPATH, "//*[@id='SearchInfo_RegistDateFrom']")
+            )
+        ).send_keys(f"{date_selection_start}")
+        print("Step 9 Inserted date start: %s" % date_selection_start)
+
+        if date_selection_end:
+            WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//*[@id='SearchInfo_RegistDateTo']")
+                )
+            ).send_keys(f"{date_selection_end}")
+            print("Step 9 Inserted date end: %s" % date_selection_end)
+        time.sleep(1)
     except Exception as e:
-        print(f"Step 8c An error occurred while selecting the checkbox: {e}")
+        print(f"Step 9 An error occurred while inserting date: {e}")
+
+        # WebDriverWait(browser, 10).until(
+        #     EC.presence_of_element_located(
+        #         (By.XPATH, "//*[@id='SearchInfo_RegistDateFrom']")
+        #     )
+        # ).send_keys(Keys.ESCAPE)
+        # time.sleep(2)
+    """
+    Step 10: Click apply search button
+    """
+    try:
+        browser.find_element(By.ID, "conmasSearchButton").click()
+
+        # WebDriverWait(browser, 10).until(
+        #     EC.presence_of_element_located((By.ID, "conmasSearchButton"))
+        # ).click()
+        time.sleep(2)
+        print("Step 10 Apply search button clicked")
+    except Exception as e:
+        print(f"Step 10 Apply search button click failed: {e}")
+
+        # WebDriverWait(browser, 10).until(
+        #     EC.presence_of_element_located(
+        #         (By.XPATH, "//*[@id='SearchDialog']/div/div[1]/span/img")
+        #     )
+        # ).click()
+    # try:
+    #     wait_until_page_loads(browser)
+
+    #     browser.find_element(By.XPATH, "//*[@id='SearchDialog']/div/div[1]/span/img").click()
+    # except Exception as e:
+    #     print(f"An error occurred while clicking the close button: {e}")
+    """
+    Step : Click the search form to apply the search
+    """
+    # try:
+    #     WebDriverWait(browser, 20).until(EC.invisibility_of_element_located((By.CLASS_NAME, "blockUI")))
+
+    #     WebDriverWait(browser, 10).until(
+    #         EC.element_to_be_clickable((By.CLASS_NAME, "img_search"))
+    #     ).click()
+    #     print("Step 11 Search button clicked")
+    # except Exception as e:
+    #     print(f"Step 11 An error occurred while clicking the search button: {e}")
+    #     sys.exit()
+
+    """
+    Step 11: Click all the checbox from this output
+    """
+    try:
+        # browser.find_element(By.XPATH, "//label[@for='IsAllOutput']").click()
+        WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//label[@for='IsAllOutput']"))
+        ).click()
+        print("Step 11 Success to select the checkbox 'ID'")
+    except Exception as e:
+        print(f"Step 11 An error occurred while selecting the checkbox: {e}")
         sys.exit()
 
     """
-    Step 9: After select the form that want to exported
+    Step 12: After select the form that want to exported
     Then click the Next button to proceed the request
     """
     try:
         WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.CLASS_NAME, "img_next"))
         ).click()
-        print("Step 9 Success to select the form that want to")
+        print("Step 12 Success to select the form that want to")
     except Exception as e:
-        print(f"Step 9 An error occurred while clicking the Next button: {e}")
+        print(f"Step 12 An error occurred while clicking the Next button: {e}")
         sys.exit()
 
     """
-    Step 10: Click the Export button to download the report
+    Step 13: Click the Next button to finalize the build report
     """
     try:
         WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.CLASS_NAME, "img_next"))
         ).click()
-        print("Step 10: Success to click the Next button at the Details page")
+        print("Step 13 Success click Next button")
     except Exception as e:
         print(
-            f"Step 10 An error occurred while clicking the Next button at Details Page: {e}"
+            f"Step 13 An error occurred while clicking the Next button at Details Page: {e}"
         )
         sys.exit()
 
     """
-    Step 11: After download the report, check if the file exists
+    Step 14: After download the report, check if the file exists
     """
     try:
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "img_output_csv"))
         ).click()
-        print("Step 11: Success to find the OUTPUT button")
+        print("Step 14: Success to find the OUTPUT button")
+        print(f"\nDownload started {10*'*'}")
     except Exception as e:
-        print(f"Step 11 An error occurred while finding the OUTPUT button: {e}")
+        print(f"Step 14 An error occurred while finding the OUTPUT button: {e}")
         sys.exit()
 
 
@@ -276,12 +308,15 @@ def wait_until_page_loads(browser, timeout=15):
 
 def monitor_download():
     print("Waiting for the download to complete...")
+    # Ensure the zip_folder exists
+    if not os.path.exists(zip_dir):
+        os.makedirs(zip_dir)
 
     # Wait until the file appears in the download directory
     while True:
-        for file in os.listdir(download_dir):
+        for file in os.listdir(zip_dir):
             if file.endswith(".zip"):
-                zip_path = os.path.join(download_dir, file)
+                zip_path = os.path.join(zip_dir, file)
                 print(f"Download complete: {file}")
 
                 # Extract the zip file
@@ -309,29 +344,55 @@ def extract_zip(zip_path, extract_to):
 
 # Main function to run the automation
 def main():
-    browser = initialize_browser()
+    print("Starting process...\n")
+    # for month in range(1, 13):
+    for month in range(1,13):
+        print(f"Processing month: {calendar.month_name[month]}")
+        # Get the first day of the month
+        date_selection_start = f"2024/{month:02d}/01"
 
-    try:
-        start_time = time.time()
-        login(browser)
-        navigate_and_perform_tasks(browser)
-        monitor_download()
+        # Get the last day of the month
+        _, last_day = calendar.monthrange(2024, month)
+        date_selection_end = f"2024/{month:02d}/{last_day:02d}"
 
-        input_directory = os.path.join(download_dir, "extracted_files")
-        output_csv = os.path.join(download_dir, "merged_output.csv")
+        try:
+            browser = initialize_browser()  # Move from the initial started
 
+            start_time = time.time()
+            login(browser)
+            navigate_and_perform_tasks(
+                browser, date_selection_start, date_selection_end
+            )
+            monitor_download()
+
+            input_directory = os.path.join(download_dir, "extracted_files", f"month_{month:02d}")
+            os.makedirs(input_directory, exist_ok=True)
+            output_csv = os.path.join("./result", f"merged_output_{month:02d}.csv")
+
+
+            #clean_directories(
+            #    ["./extracted_files", zip_dir]
+            #)  # This section is used to clean the directory
+            elapsed_time = time.time() - start_time
+            print(
+                f"Automation for {date_selection_start} to {date_selection_end} completed successfully. Elapsed time: {elapsed_time} seconds"
+            )
+            browser.quit()
+        except Exception as e:
+            print(f"An error occurred for {calendar.month_name[month]}: {e}")
+        finally:
+            browser.quit()
+
+    # Merge CSV files after the loop
+    for month in range(1,13):
+        input_directory = os.path.join(download_dir, "extracted_files", f"month_{month:02d}")
+        output_csv = os.path.join("./result", "merged_output_total.csv")
         merge_csv_files(input_directory, output_csv)
 
-        elapsed_time = time.time() - start_time
-        print(
-            f"Automation completed successfully. Elapsed time: {elapsed_time} seconds"
-        )
-
-        clean_directory(["./extracted_files", download_dir])
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        browser.quit()
+    export_to_hyper(
+        "./result/merged_output_total.csv",
+        filename="pcb_molding_total.hyper",
+    )
 
 
 if __name__ == "__main__":
